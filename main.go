2db4ad5c63acf3f7575643313dbebef8c2db4285
Ivanov” Yevhen <ivanovev77@gmail.com>
2025-02-07 18:30:57 +0200

    second commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,6 +13,32 @@ import (
 type loggingRoundTripper struct {
 	logger io.Writer
 	next   http.RoundTripper
+}
+
+type assetsResponce struct {
+	Assets    []assetData `json:"data"`
+	Timestamp int64       `json:"timestamp"`
+}
+
+// type assetResponce struct {
+// 	Asset     assetData `json:"data"`
+// 	Timestamp int64     `json:"timestamp"`
+// }
+
+func (d assetData) Info() string {
+	return fmt.Sprintf("[ID] %s | [RANK] %s | [SYMBOL] %s [NAME] %s [PRICE]", d.ID, d.Rank, d.Name, d.PriceUsd)
+}
+
+type assetData struct {
+	ID            string `json:"id"`
+	Rank          string `json:"rank"`
+	Symbol        string `json:"symbol"`
+	Name          string `json:"name"`
+	Supply        string `json:"supply"`
+	MaxSupply     string `json:"max"`
+	MarketCapUsd  string `json:"marketCapUsd"`
+	VolumeUsd24Hr string `json:"volumeUsd24Hr"`
+	PriceUsd      string `json:"priceUsd"`
 }
 
 func (l loggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -32,7 +59,7 @@ func main() {
 		},
 		Timeout: 10 * time.Second,
 	}
-	resp, err := client.Get("https://djinni.co/my/dashboard/")
+	resp, err := client.Get("https://api.coincap.io/v2/assets")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,5 +72,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Response body:", string(body))
+	// fmt.Println("Response body:", string(body))
+	var r assetsResponce
+	if err = json.Unmarshal(body, &r); err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range r.Assets {
+		fmt.Println(v.Info())
+	}
 }
